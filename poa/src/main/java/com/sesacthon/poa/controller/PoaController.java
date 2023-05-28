da5842443041a3ef8aa821dd038f8b95f138ba8d
Su-Yeo <yyy1247@gmail.com>
2023-05-28 21:15:34 +0900

    init

---
+++ poa/src/main/java/com/sesacthon/poa/controller/PoaController.java
@@ -0,0 +1,23 @@
+package com.sesacthon.poa.controller;
+
+import com.sesacthon.poa.service.PoaService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@RestController
+@Slf4j
+public class PoaController {
+    private final PoaService poaService;
+
+    @ResponseBody
+    @GetMapping("/test")
+    public Object selectData(){
+        return "test";
+    }
+}
