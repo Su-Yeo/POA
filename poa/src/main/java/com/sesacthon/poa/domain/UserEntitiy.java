9dc7591e3902ddcb8c0ac67462521a38c819276d
Su-Yeo <yyy1247@gmail.com>
2023-05-29 14:46:38 +0900

    entity 예제

---
+++ poa/src/main/java/com/sesacthon/poa/domain/UserEntitiy.java
@@ -0,0 +1,57 @@
+package com.sesacthon.poa.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "User")
+public class UserEntitiy {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer user_id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String pw;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String nick;
+
+    @Column(nullable = false)
+    private String profile;
+
+    private String address;
+
+    private Integer creator_id;
+
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime create_time;
+
+    @LastModifiedDate
+    private LocalDateTime update_time;
+
+    private LocalDateTime delete_time;
+}
