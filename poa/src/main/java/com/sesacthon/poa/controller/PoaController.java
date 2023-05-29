665a476fd64178ebb7adee1478604cdede6dc35b
Su-Yeo <yyy1247@gmail.com>
2023-05-29 17:23:22 +0900

    주석

--- poa/src/main/java/com/sesacthon/poa/controller/PoaController.java
+++ poa/src/main/java/com/sesacthon/poa/controller/PoaController.java
@@ -4,21 +4,23 @@ import com.sesacthon.poa.dto.UserDto;
 import com.sesacthon.poa.service.PoaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
 @Slf4j
 public class PoaController {
-    private final PoaService poaService;
+    private final PoaService poaService; // 추후 서비스 별로 나눠야 함
 
+    /**
+     * user_id로 유저 조회
+     * @param user_id
+     * @return UserDto
+     */
     @ResponseBody
     @GetMapping("/user")
-    public UserDto findUser(){
-        return poaService.findUser(1);
+    public UserDto findUser(@RequestBody Integer user_id){
+        return poaService.findUser(user_id);
     }
 }
--- poa/src/main/java/com/sesacthon/poa/service/PoaService.java
+++ poa/src/main/java/com/sesacthon/poa/service/PoaService.java
@@ -11,9 +11,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class PoaService {
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
+    private final UserRepository userRepository; // JPA
+    private final UserMapper userMapper; // DTO로 변환
 
+    /**
+     * user_id로 유저 조회
+     * @param user_id
+     * @return UserDto
+     */
     public UserDto findUser(Integer user_id){
         return userMapper.toDto(userRepository.findById(user_id).orElse(null));
     }
