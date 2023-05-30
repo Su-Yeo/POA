e640df9521a2d092c9b725548c7c35e0188c773c
Su-Yeo <yyy1247@gmail.com>
2023-05-31 00:04:45 +0900

    Entity, Dto, Mapper 생성 - 유저, 작가 정보, 장애 정보, 작품

--- poa/src/main/java/com/sesacthon/poa/dto/mapper/UserMapper.java
+++ poa/src/main/java/com/sesacthon/poa/dto/mapper/UserMapper.java
@@ -1,9 +1,9 @@
 package com.sesacthon.poa.dto.mapper;
 
-import com.sesacthon.poa.domain.DisabledEntity;
+import com.sesacthon.poa.domain.UserEntity;
 import com.sesacthon.poa.dto.UserDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper extends StructMapper<UserDto, DisabledEntity> {
+public interface UserMapper extends StructMapper<UserDto, UserEntity> {
 }
