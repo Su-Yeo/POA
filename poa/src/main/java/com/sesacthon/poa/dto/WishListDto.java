ccd37b381d2740d23bc6ca92c1de4e7b7c9c9d72
golldymom <khnam5@naver.com>
2023-05-31 17:35:49 +0900

    ignore test

---
+++ poa/src/main/java/com/sesacthon/poa/dto/WishListDto.java
@@ -0,0 +1,19 @@
+package com.sesacthon.poa.dto;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class WishListDto { // 좋아요 Dto
+
+    private Integer wishlist_id;
+
+    private Integer artwork_id;
+
+    private Integer user_id;
+
+
+
+}
